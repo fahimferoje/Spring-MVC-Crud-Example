b09b1795499c87549f3596e2dc01abcde24b9dff
fahimferoje <fabrahamjohn@gmail.com>
2020-05-10 14:14:13 +0600

    configs

---
+++ src/main/java/com/crud/config/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.crud.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {WebMvcConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	
+}
